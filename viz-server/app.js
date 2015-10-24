4e8581a9c8ff33407cda842550e8fc3f096bfceb
Scott Ritchie <sritchie73@gmail.com>
2015-10-24 12:28:00 +1100

    cleaning up directory structure and removing non Shiny-app data and scripts

--- viz-server/app.js
+++
@@ -1,5 +0,0 @@
-var server = require('./server');
-
-console.log("Starting web server...");
-server.start();
-console.log("Successfully started web server. Waiting for incoming connections...");
--- viz-server/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "mongodb": {
-		"connectionstring": process.env.MONGODB || "mongodb://localhost:27017/test"
-	},
-	"logger": {
-		"api": "logs/api.log",
-		"exception": "logs/exceptions.log"
-	}
-};
